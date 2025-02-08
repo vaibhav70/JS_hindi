9f952dc91428f934ef566c407440c4042e4e8243
vaibhav70 <48535971+vaibhav70@users.noreply.github.com>
2025-02-08 13:27:07 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accId = 199112
+let mailId = "vaibhav@gmail.com"
+var password = "12455"
+accCity = "Jaipur"
+let limit
+let nullValue = null;
+
+//avoid using var because of scoping issue
+// use let instead
+
+// accId = 2 // not allowed
+mail = "a@g.com"
+password = "2222"
+accCity = "Bengaluru"
+
+console.table([accId, mail, password, accCity, limit, nullValue]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // js engine will treat this file as newer version of js
+
+// alert(9) // not supported in node, but supported in browser
+
+let name = "vaibhav"
+let age = 22
+let isLoggedIn = true
+
+//number 2 to 2^53
+// bigint
+// string > "sjjsjs"
+// boolean > true / false
+// null > standalone value
+// undefined > variable havent been defined yet
+// symbol > unique
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
